b89ace3c1127db98fe3e7d20ad6627dc824a65da
Cristiano Silva Jr <crisjr@pm.me>
2021-04-28 16:59:34 -0300

    Setup basic Telegram bot

--- app/main.rb
+++ app/main.rb
@@ -1,11 +1,31 @@
 require 'yaml'
+require 'telegram_bot'
 
 class App
   def initialize config
     @config = config
+    @bot = TelegramBot.new token: @config['api_token']
   end
 
   def run
+    puts "# TODO List bot"
+    @bot.get_updates do |message|
+      puts "---"
+      puts "from: @#{message.from.username}"
+      puts "message: #{message.text}"
+      command = message.get_command_for @bot
+
+      message.reply do |reply|
+        case command
+        when '/start'
+          reply.text = "Hello!"
+        else
+          reply.text = "Sorry, not implemented yet"
+        end
+        puts "reply #{reply.text.inspect}"
+        reply.send_with @bot
+      end
+    end
   end
 end
 
